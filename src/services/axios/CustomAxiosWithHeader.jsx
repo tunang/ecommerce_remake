090992babfa7b028b5818ab0410b4b726c4b4556
tunang <nguyentuan20042207@gmail.com>
2024-08-10 22:41:09 +0700

    test

--- src/services/axios/CustomAxiosWithHeader.jsx
+++ src/services/axios/CustomAxiosWithHeader.jsx
@@ -103,7 +103,7 @@ const setupAxiosInterceptors = () => {
             { refreshToken: RefreshToken }
           );
 
-          console.log(newResponse);
+          console.log(newResponse.data.message);
 
           localStorage.setItem(
             "AccessToken",
